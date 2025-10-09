cfe574f9ddfe142d903cad8de51531742461900b
paula-nourished <91964207+paula-nourished@users.noreply.github.com>
2025-10-09 15:46:51 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
 'use client';
 import dynamic from 'next/dynamic';
-const QuizClient = dynamic(() => import('./QuizClient'), { ssr: false });
+const QuizClient = dynamic(() => import('./QuizClient.jsx'), { ssr: false });
 export default function Page(){return <div style={{minHeight:300,display:'grid',placeItems:'center'}}><QuizClient/></div>;}
